bf7fec3d60a3f8dc58e3918304f845a9483b6adb
Dreamwave7 <dima190179@gmail.com>
2023-04-16 22:21:26 +0300

    fact

---
+++ factorize.py
@@ -0,0 +1,30 @@
+from multiprocessing import cpu_count
+from time import time
+
+def factorize(*number):
+    result = []
+    for num in number:
+        if num % 2 == 0: #Парне
+            x = []
+            for i in range(1,num+2,1):
+                if num % i == 0:
+                    x.append(i)
+            if bool(x):
+                result.append(x)
+
+        if num % 2 == 1: #Не парне
+            x = []
+            for i in range(1,num+2,2):
+                if num % i == 0:
+                    x.append(i)
+            if bool(x):
+                result.append(x)
+    return result
+
+
+if __name__ == "__main__":
+    timer = time()
+    a,b,c,d = factorize(128,255,99999,10651060)
+    print(time()- timer)
+    #print(cpu_count())   #4ядра
+
